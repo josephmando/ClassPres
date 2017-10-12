0e87959e8410608b2649ebb1c1787cb61db524c7
josephmando <josephmando-1@ou.edu>
2017-10-11 19:01:15 -0500

    Added Plotting

--- Class.R
+++ Class.R
@@ -10,3 +10,11 @@ write.xlsx(my_df, file="my_df.xlsx")
 
 install.packages("ggplot2")
 library(ggplot2)
+
+ggplot(data = my_df, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth() +
+  labs(y= "Highway mileage/gallon", 
+       x=  "Engine size",
+       title = "Car Mileage Comparison") +
+  theme_dark()
